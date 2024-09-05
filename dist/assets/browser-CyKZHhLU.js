0e9c7bb56d1473d732f9b78e7be44b2c34691224
reyrot24 <reyrotaru1@gmail.com>
2024-09-05 20:31:48 +0200

    Adding dist

--- dist/assets/browser-CyKZHhLU.js
+++ dist/assets/browser-CyKZHhLU.js
@@ -1,4 +1,4 @@
-import{c as Pe,g as Xe}from"./index-BzKRIxPE.js";var se={exports:{}};/** @license
+import{c as Pe,g as Xe}from"./index-BamTh0_b.js";var se={exports:{}};/** @license
  * eventsource.js
  * Available under MIT License (MIT)
  * https://github.com/Yaffle/EventSource/
